2f99c93b3bc4134f0cde5044ecb9444678809652
Akgnbas <irem.bas@std.bogazici.edu.tr>
2025-10-31 16:59:05 +0300

    Genel kodlar ve q1

---
+++ HW2.py
@@ -0,0 +1,118 @@
+import csv 
+import numpy as np
+import math
+
+
+coordinates="coordinates_10.csv"
+demands="demand_10.csv"
+costs="costs_10.csv"
+
+epsilon=1e-8 
+
+
+
+def load_data():
+
+    # Load customer coordinates (a_j)
+    customer_coords = []
+    with open(coordinates, 'r') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            customer_coords.append([float(row[0]), float(row[1])])
+    customer_coords = np.array(customer_coords)
+    n = customer_coords.shape[0]
+
+    # Load customer demand (h_j)
+    demand = []
+    with open(demands, 'r') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            demand.append(float(row[0]))
+    demand = np.array(demand)
+
+     # Load unit transport costs (c_ij)
+    unit_costs = []
+    with open(costs, 'r') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            unit_costs.append([float(c) for c in row])
+    unit_costs = np.array(unit_costs)
+    m = unit_costs.shape[0]
+
+
+    # Calculate total transport cost C_ij = h_j * c_ij
+    transport_costs = unit_costs * demand
+
+    print(f"Number of customers (n): {n}")
+    print(f"Number of facilities (m): {m}")
+    print("-" * 30)
+    
+    return n, m, customer_coords, demand, unit_costs, transport_costs
+
+
+# Distance Functions 
+
+def squared_euclidean_distance(p1, p2):
+    return np.sum((p1 - p2)**2)
+
+def euclidean_distance(p1, p2, epsilon):
+    return math.sqrt(np.sum((p1 - p2)**2)) + epsilon  # To prevent division by zero 
+
+
+# Q1
+
+def solve_part1(facility_index, customer_coords, transport_costs):
+
+    # Get the transport costs (C_ij) for the chosen facility
+    C_j = transport_costs[facility_index, :]
+
+    # Calculate the optimal location by formulas
+    # x_1* = (sum(C_j * a_1j)) / sum(C_j)
+    # x_2* = (sum(C_j * a_2j)) / sum(C_j)
+   
+    sum_C_j = np.sum(C_j) 
+
+    # This calculates [sum(C_j * a_1j), sum(C_j * a_2j)]
+    weighted_coords_sum = np.dot(C_j, customer_coords)
+    # Division in formula
+    optimal_location = weighted_coords_sum / sum_C_j
+
+    # Calculate the total objective cost
+    total_cost = 0.0
+    for j in range(customer_coords.shape[0]):
+        dist_sq = squared_euclidean_distance(optimal_location, customer_coords[j])
+        total_cost += C_j[j] * dist_sq
+    
+    # Outputs
+
+    print(f"Optimal Location: ({optimal_location[0]:.2f}, {optimal_location[1]:.2f})")
+    print(f"Minimum Total Cost: {total_cost:,.2f}")
+    
+    return optimal_location, total_cost
+
+
+# --- Main Execution ---
+
+if __name__ == "__main__":
+        # Load all data
+        n, m, customer_coords, demand, unit_costs, transport_costs = load_data()
+        
+        # --- Part 1 ---
+        print("\n=== Q 1: Single Facility (Squared Euclidean) ===")
+        # We chose Facility 1 (index 0)
+        FACILITY_TO_TEST = 0
+        part1_location, part1_cost = solve_part1(FACILITY_TO_TEST, customer_coords, transport_costs)
+    
+
+
+
+
+
+
+
+
+    
+
+
+
+    
